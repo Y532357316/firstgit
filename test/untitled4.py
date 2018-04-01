23bea1a7e4b1628833c8dfc087a267ebf7a22621
y532357316 <532357316@qq.com>
2018-04-01 23:02:20 +0800

    Signed-off-by: y532357316 <532357316@qq.com>

---
+++ test/untitled4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 13:09:03 2018
+
+@author: 带心情去旅行
+"""
+str1 = input("请输入一个人的名字：")
+str2 = input("请输入一个国家的名字：")
+print("世界这么大，{}想去{}看看。".format(str1,str2))
+
+
+
+      
